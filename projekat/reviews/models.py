bdd26b54e015030d0669250ec5071b0919e25694
Cibowa-Caluz <zecevic.luka@gmail.com>
2021-01-02 17:58:26 +0100

    migracije

--- projekat/reviews/models.py
+++ projekat/reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    content = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.content
+
+
+class Review(models.Model):
+    content = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
